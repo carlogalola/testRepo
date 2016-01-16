8edfa513520697001a6e9f0d5a1c4b56b09c41fb
carlogalola <carlo.galola@gmail.com>
2016-01-16 22:15:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
 <?php if (have_posts()) : ?>
 <?php while (have_posts()) : the_post(); ?>
 
-<div class="post" id="post-<?php the_ID(); ?>">
-
 <div class="title">
 	<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 	<div class="titlemeta">
